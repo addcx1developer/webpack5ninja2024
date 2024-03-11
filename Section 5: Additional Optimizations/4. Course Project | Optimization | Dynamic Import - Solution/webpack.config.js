f84b6d87b32ca72b9f8f7baab05b61138873122f
addcx1developer <addcx1developer@gmail.com>
2024-03-11 14:30:46 +0800

    Section 5: Additional Optimizations - 4. Course Project | Optimization | Dynamic Import - Solution | Adding Hash to Output Bundle Files

--- Section 5: Additional Optimizations/4. Course Project | Optimization | Dynamic Import - Solution/webpack.config.js
+++ Section 5: Additional Optimizations/4. Course Project | Optimization | Dynamic Import - Solution/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
 		courses: './src/pages/courses.js',
 	},
 	output: {
-		filename: '[name].bundle.js',
+		filename: '[name].[contenthash].js',
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
 	},
@@ -53,7 +53,7 @@ module.exports = {
 				},
 			],
 		}),
-		// new BundleAnalyzerPlugin({}),
+		new BundleAnalyzerPlugin({}),
 	],
 	optimization: {
 		splitChunks: {
